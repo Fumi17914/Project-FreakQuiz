979555ce209685825514b4d25f8852f221ba41cb
José Fumanal <jfumanal92@gmail.com>
2020-12-02 17:02:07 +0100

    Initial Commit

---
+++ Project FreakQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project FreakQuiz
+//
+//  Created by Jose Fumanal Quintana on 2/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
